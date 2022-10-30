0076856ec28111c7765d8ba897e68fb2f283debe
Richard Schneider <richard@schneiderbox.net>
2022-10-30 17:42:31 -0500

    Link to account detail from account list

--- unclebudget/urls.py
+++ unclebudget/urls.py
@@ -5,7 +5,7 @@ from unclebudget.views import *
 
 urlpatterns = [ 
     path('accounts', AccountList.as_view()),
-    path('accounts/<int:id>', AccountDetail.as_view()),
+    path('accounts/<int:pk>', AccountDetail.as_view(), name='accounts-list'),
     path('envelopes', EnvelopeList.as_view()),
-    path('envelopes/<int:id>', EnvelopeDetail.as_view()),
+    path('envelopes/<int:pk>', EnvelopeDetail.as_view()),
 ]
