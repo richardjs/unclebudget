335b157bca0379b63b183f313befdceb0f8c3176
Richard Schneider <richard@schneiderbox.net>
2023-01-06 22:57:27 -0600

    Make URLs more consistent

--- unclebudget/urls.py
+++ unclebudget/urls.py
@@ -6,10 +6,10 @@ from unclebudget.views import *
 urlpatterns = [ 
     path('', summary, name='summary'),
 
-    path('accounts/<int:pk>', account_detail, name='account-detail'),
-    path('envelopes/<int:pk>', envelope_detail, name='envelope-detail'),
-    path('envelopes/new', EnvelopeCreateView.as_view(), name='envelope-create'),
+    path('account/<int:pk>', account_detail, name='account-detail'),
     path('entry/<int:pk>', entry_detail, name='entry-detail'),
+    path('envelope/<int:pk>', envelope_detail, name='envelope-detail'),
+    path('envelope/new', EnvelopeCreateView.as_view(), name='envelope-create'),
 
     path('process', process, name='process'),
     path('upload', upload, name='upload'),
