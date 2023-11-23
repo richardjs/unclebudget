e1240f7cf8e6fb457509f139345ce48a07f67206
Richard Schneider <richard@schneiderbox.net>
2023-11-23 16:44:12 -0600

    Update deployment config for poetry

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-python manage.py migrate
-python manage.py collectstatic --no-input
-python manage.py check --deploy
-gunicorn -b 0.0.0.0:8000 project.wsgi
+poetry run python manage.py migrate
+poetry run python manage.py createcachetable
+poetry run python manage.py collectstatic --no-input
+poetry run python manage.py check --deploy
+poetry run gunicorn -b 0.0.0.0:8000 project.wsgi
